3fffe70f96ae0876386e33979f55b37a75d5e681
Misael <misaelborges1981@gmail.com>
2025-08-16 10:32:31 -0300

    Agendamentos

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,11 @@
+import Image from "next/image"
 import Header from "./_components/Header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import { SearchIcon } from "lucide-react"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 export default function Home() {
   return (
@@ -7,6 +14,42 @@ export default function Home() {
       <div className="p-5">
         <h2 className="txt-xl font-bold">Olá, Misael!</h2>
         <p>Sábado, 16 de Agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+            alt="Banner principal - Agende com os melhores"
+          />
+        </div>
+
+        <Card className="mt-6 p-0">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">16</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
